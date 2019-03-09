8b33f77a90fedd5d6fe9351cbfcdf6cee4d41841
Kai Shoya <s-kai@lumber-mill.co.jp>
2019-03-09 22:30:02 +0900

    チャット画面の見た目を少しだけSlack風に... coffee嫌いなのでjsに置換

---
+++ app/assets/javascripts/channels/room.js
@@ -0,0 +1,34 @@
+App.room = App.cable.subscriptions.create("RoomChannel", {
+  connected: function() {
+    // Called when the subscription is ready for use on the server
+  },
+
+  disconnected: function() {
+    // Called when the subscription has been terminated by the server
+  },
+
+  received: function(data) {
+    $('#messages').append(data['message']);
+  },
+
+  speak: function(message) {
+    this.perform('speak', {message: message});
+  }
+}, $(document).on('keypress', '[data-behavior~=room_speaker]', function(event) {
+  if (event.keyCode === 13) {
+    if (event.target.value === undefined || event.target.value === '') {return;}
+    App.room.speak(event.target.value);
+    event.target.value = '';
+    event.preventDefault();
+  }
+}));
+
+$(function() {
+  // 送信ボタン押下時の処理
+  $('#submit_btn').on('click', function() {
+    if (event.target.value === undefined || event.target.value === '') {return;}
+    var element = $('#input_msg');
+    App.room.speak(element.val());
+    element.val('');
+  });
+});
