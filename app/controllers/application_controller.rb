f8efee8d605b262682a85b0f8b41eb1ad35881ac
Kai Shoya <s-kai@lumber-mill.co.jp>
2019-03-09 23:44:17 +0900

    サインアップ・サインインにemailの代わりにusernameを使うようにしました

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  def configure_permitted_parameters
+    #strong parametersを設定し、usernameを許可
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:username, :password, :password_confirmation)
+    end
+    devise_parameter_sanitizer.permit(:sign_in) do |u|
+      u.permit(:username, :password, :remember_me)
+    end
+  end  
 end
